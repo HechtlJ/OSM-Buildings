ba04f7946ef1e46172766bb07910071c2f6e6c47
Johannes Hechtl <johannes.hechtl@tum.de>
2021-05-05 13:08:02 +0200

    added my_command, that draws a new line in the active document

--- InitGui.py
+++ InitGui.py
@@ -1,5 +1,5 @@
 import FreeCAD, FreeCADGui 
- 
+
 class ScriptCmd: 
    def Activated(self): 
        # Here your write what your ScriptCmd does...
@@ -11,13 +11,39 @@ FreeCADGui.addCommand('Script_Cmd', ScriptCmd())
 
 
 
+class OSM_Buildings (Workbench):
 
-class OSM_Buildings (Workbench): 
     MenuText = "OSM-Buildings"
+    ToolTip = "A description of my workbench"
+    Icon = """paste here the contents of a 16x16 xpm icon"""
+
     def Initialize(self):
-        #import Scripts # assuming Scripts.py is your module
-        list = ['Script_Cmd', 'DemoCommand'] # That list must contain command names, that can be defined in Scripts.py
-        self.appendToolbar("My Scripts",list) 
+        """This function is executed when FreeCAD starts"""
+        #import MyModuleA, MyModuleB # import here all the needed files that create your FreeCAD commands
+        import my_test
+        self.list = ['Script_Cmd', 'My_Command'] # A list of command names created in the line above
+        self.appendToolbar("My Commands",self.list) # creates a new toolbar with your commands
+        self.appendMenu("My New Menu",self.list) # creates a new menu
+        self.appendMenu(["An existing Menu","My submenu"],self.list) # appends a submenu to an existing menu
+
+    def Activated(self):
+        """This function is executed when the workbench is activated"""
+        return
+
+    def Deactivated(self):
+        """This function is executed when the workbench is deactivated"""
+        return
+
+    def ContextMenu(self, recipient):
+        """This is executed whenever the user right-clicks on screen"""
+        # "recipient" will be either "view" or "tree"
+        self.appendContextMenu("My commands",self.list) # add commands to the context menu
+
+    def GetClassName(self): 
+        # This function is mandatory if this is a full python workbench
+        # This is not a template, the returned string should be exactly "Gui::PythonWorkbench"
+        return "Gui::PythonWorkbench"
+       
 
         
 Gui.addWorkbench(OSM_Buildings())
---
+++ my_test.py
@@ -0,0 +1,30 @@
+import FreeCAD, FreeCADGui 
+import Part,PartGui 
+
+
+class My_Command_Class():
+    """My new command"""
+
+    def GetResources(self):
+        return {'Pixmap'  : 'My_Command_Icon', # the name of a svg file available in the resources
+                'Accel' : "Shift+S", # a default shortcut (optional)
+                'MenuText': "My New Command",
+                'ToolTip' : "What my new command does"}
+
+    def Activated(self):
+        """Do something here"""
+        doc=FreeCAD.activeDocument() 
+        # add a line element to the document and set its points 
+        l=Part.LineSegment()
+        l.StartPoint=(0.0,0.0,0.0)
+        l.EndPoint=(1.0,1.0,1.0)
+        doc.addObject("Part::Feature","Line").Shape=l.toShape() 
+        doc.recompute()
+        return
+
+    def IsActive(self):
+        """Here you can define if the command must be active or not (greyed) if certain conditions
+        are met or not. This function is optional."""
+        return True
+
+FreeCADGui.addCommand('My_Command',My_Command_Class())
