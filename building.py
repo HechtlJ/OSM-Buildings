70fd627797526d731ec889d01c175a63e3a833e3
Johannes Hechtl <johannes.hechtl@tum.de>
2021-06-25 09:23:04 +0200

    the height is now dependent on the amount of levels

--- building.py
+++ building.py
@@ -1,12 +1,17 @@
 
+HEIGHT_PER_LEVEL = 3
 
 class Building:
     """ This class represents a building. It saves all the data of a building."""
 
     def __init__(self):
-        self.height = 0
-        self.points = []
+        
+        self.points = []    # The corner points of the building. Each point is a dictionary with a x and a y value
         self.address = ""
+        self.levels = 3 
+
+        self.height = HEIGHT_PER_LEVEL * self.levels 
+
 
 
     def add_point(self, x, y):
@@ -15,6 +20,12 @@ class Building:
 
     def set_address(self, address):
         self.address = address
+
+    def set_levels(self, levels):
+        self.levels = int(levels)
+        if self.levels < 1:
+            self.levels = 1
+        self.height = self.levels * HEIGHT_PER_LEVEL
 
 
     
